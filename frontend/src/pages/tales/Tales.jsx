3722fcfc7451077fc7364de4ff35317704764d63
FREDERIC TICON <fred.ticon@gmail.com>
2023-11-23 16:05:45 +0100

    Create Tales.jsx

---
+++ frontend/src/pages/tales/Tales.jsx
@@ -0,0 +1,13 @@
+import Text from "../../components/text/Text";
+import "./Tales.scss";
+
+function Tales() {
+  return (
+    <main>
+      <div className="imagefond"></div>
+      <Text />
+    </main>
+  );
+}
+
+export default Tales;
