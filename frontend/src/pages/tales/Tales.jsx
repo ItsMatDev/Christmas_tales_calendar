8aba4984fd10d7f04e0417f744e490bba3c63ef3
VincentRssx <Vincent.rssx59@gmail.com>
2023-11-23 19:49:57 +0100

    test animation

--- frontend/src/pages/tales/Tales.jsx
+++ frontend/src/pages/tales/Tales.jsx
@@ -2,24 +2,37 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Text from "../../components/text/Text";
 import "./Tales.scss";
+import noel from "../../assets/perenoel.png";
 
 function Tales() {
-  const { id } = useParams();
-  const [Calendarcase, setCalendarcase] = useState();
+	const { id } = useParams();
+	const [Calendarcase, setCalendarcase] = useState();
 
-  useEffect(() => {
-    fetch("http://localhost:5000/noel")
-      .then((response) => response.json())
-      .then((data) => setCalendarcase(data));
-  }, []);
+	useEffect(() => {
+		fetch("http://localhost:5000/noel")
+			.then((response) => response.json())
+			.then((data) => setCalendarcase(data));
+	}, []);
 
-  const calendarc = (Calendarcase || []).find((el) => el.id === parseInt(id, 10));
+	useEffect(() => {
+		document.body.style.overflow = "hidden";
+		return () => {
+			document.body.style.overflow = "visible";
+		};
+	}, []);
 
-  return (
-    <main className="tales__page">
-      <Text />
-    </main>
-  );
+	const calendarc = (Calendarcase || []).find(
+		(el) => el.id === parseInt(id, 10)
+	);
+
+	return (
+		<>
+			<img src={noel} alt="" />
+			<main className="tales__page">
+				<Text />
+			</main>
+		</>
+	);
 }
 
 export default Tales;
