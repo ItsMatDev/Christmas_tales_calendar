61482fddd97013d802c6912358783fbd9eea7dfa
VincentRssx <Vincent.rssx59@gmail.com>
2023-11-23 15:34:14 +0100

    ajout du grid et du map aléatoire

---
+++ frontend/src/components/Calendar/Calendarcase.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+import "./Calendarcase.scss";
+
+function Calendarcase() {
+	const [Calendarcase, setCalendarcase] = useState();
+
+	useEffect(() => {
+		fetch("http://localhost:5000/noel")
+			.then((response) => response.json())
+			.then((data) => setCalendarcase(data));
+	}, []);
+
+	console.info(Calendarcase);
+
+	const genererIndiceAleatoire = () => {
+		if (Calendarcase) {
+			const indiceAleatoire = Math.floor(
+				Math.random() * Calendarcase.length
+			);
+			return indiceAleatoire;
+		}
+		return 0;
+	};
+
+	return (
+		<div className="parent">
+			{Calendarcase &&
+				Calendarcase.map((calendarcas) => {
+					const indiceAleatoire = genererIndiceAleatoire();
+
+					return (
+						<section
+							key={calendarcas.id}
+							className={calendarcas.Name}
+							style={{ order: indiceAleatoire }}
+						>
+							{calendarcas.id}
+						</section>
+					);
+				})}
+		</div>
+	);
+}
+
+export default Calendarcase;
