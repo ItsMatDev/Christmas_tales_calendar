1193aa4e3a4b4e39a97f66fed003293fa2e7d134
ItsMatDev <thierry.matthieu@live.fr>
2023-11-23 23:36:36 +0100

    fixed audio tag

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -11,22 +11,22 @@ function Home() {
     <>
       <Smoke />
       <Snow />
-    <main className="homeMain">
-      <div className="HomeTitle">
-        <HomeTitle />
-      </div>
-      <div className="CountdownTimer">
-        <CountdownTimer />
-      </div>
-      <div className="Presentation">
-        <Presentation />
-      </div>
-      <div className="HouseHome">
-        <HouseHome />
-      </div>
-      <audio autoPlay={true} controls src="/src/sound/musicnoel.mp3"></audio>;
-    </main>
-   </>
+      <main className="homeMain">
+        <div className="HomeTitle">
+          <HomeTitle />
+        </div>
+        <div className="CountdownTimer">
+          <CountdownTimer />
+        </div>
+        <div className="Presentation">
+          <Presentation />
+        </div>
+        <div className="HouseHome">
+          <HouseHome />
+        </div>
+        <audio autoPlay={true} controls src="/src/sound/musicnoel.mp3"></audio>;
+      </main>
+    </>
   );
 }
 
