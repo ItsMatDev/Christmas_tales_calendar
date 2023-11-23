4cd0922a8882255889f60b53e3d945a49bb287f9
VincentRssx <Vincent.rssx59@gmail.com>
2023-11-23 17:32:50 +0100

    test animation

--- frontend/src/pages/Tales.jsx
+++ frontend/src/pages/Tales.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import "../pages/tales.scss";
 
 function Tales() {
 	const { id } = useParams();
@@ -16,10 +17,14 @@ function Tales() {
 	);
 
 	return (
-		<>
-			<h1>Tales</h1>
+		<div>
+			<img
+				src="https://previews.123rf.com/images/pixdesign123/pixdesign1231311/pixdesign123131100141/23664806-p%C3%A8re-no%C3%ABl-avec-son-tra%C3%AEneau.jpg"
+				alt=""
+			/>
+			<h1 className="title">Tales</h1>
 			<p>{calendarc && calendarc.name}</p>
-		</>
+		</div>
 	);
 }
 
