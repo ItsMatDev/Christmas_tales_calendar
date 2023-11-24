80fa74c3131d694c693880be126da96266e9920b
VincentRssx <Vincent.rssx59@gmail.com>
2023-11-24 10:34:45 +0100

    animation livre

--- frontend/src/components/picturetales/PictureTales.jsx
+++ frontend/src/components/picturetales/PictureTales.jsx
@@ -1,12 +1,17 @@
 import "./PictureTales.scss";
 import borderLight from "../../assets/border-light.png";
 function PictureTales({ content }) {
-  return (
-    <div>
-    <img className="border__img" src={borderLight}/>
-  <img className="pictureconte" src={`http://localhost:5010${content.imgSrc}`} alt="" />;
-  </div>
-  )
+	return (
+		<div className="picture">
+			<img className="border__img" src={borderLight} />
+			<img
+				className="pictureconte"
+				src={`http://localhost:5010${content.imgSrc}`}
+				alt=""
+			/>
+			;
+		</div>
+	);
 }
 
 export default PictureTales;
