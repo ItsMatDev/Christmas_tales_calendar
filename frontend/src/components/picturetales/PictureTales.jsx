d74286aee9754f8369874e925e439ce99dc5f363
Helene Rampelberg <h.rampelberg@yahoo.fr>
2023-11-24 03:03:05 +0100

    add border on the pic on tales

--- frontend/src/components/picturetales/PictureTales.jsx
+++ frontend/src/components/picturetales/PictureTales.jsx
@@ -1,7 +1,12 @@
 import "./PictureTales.scss";
-
+import borderLight from "../../assets/border-light.png";
 function PictureTales({ content }) {
-  return <img className="pictureconte" src={`http://localhost:5010${content.imgSrc}`} alt="" />;
+  return (
+    <div>
+    <img className="border__img" src={borderLight}/>
+  <img className="pictureconte" src={`http://localhost:5010${content.imgSrc}`} alt="" />;
+  </div>
+  )
 }
 
 export default PictureTales;
